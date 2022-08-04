4dc69b8638d7a70b720a19ce5732f2284e067328
rondadefidon <flyfreshfits@gmail.com>
2022-08-04 18:28:46 -0400

    update straw fudge

--- src/pages/Bank/components/Harvest.tsx
+++ src/pages/Bank/components/Harvest.tsx
@@ -28,8 +28,8 @@ const Harvest: React.FC<HarvestProps> = ({ bank }) => {
   const tombStats = useTombStats();
   const tShareStats = useShareStats();
 
-  const tokenName = bank.earnTokenName === 'CSHARE' ? 'CSHARE' : 'CREAM';
-  const tokenStats = bank.earnTokenName === 'CSHARE' ? tShareStats : tombStats;
+  const tokenName = bank.earnTokenName === 'STRAW' ? 'STRAW' : 'FUDGE';
+  const tokenStats = bank.earnTokenName === 'STAW' ? tShareStats : tombStats;
   const tokenPriceInDollars = useMemo(
     () => (tokenStats ? Number(tokenStats.priceInDollars).toFixed(2) : null),
     [tokenStats],
